0905ff44e7dfd0a3c41cc3e882549637d267d5b8
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-04 21:48:59 +0300

    Declare the tokens

--- token/token.go
+++ token/token.go
@@ -1,1 +1,34 @@
 package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// identifiers
+	IDENT = "IDENT"
+	INT = "INT"
+
+	// delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+
+	// operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
