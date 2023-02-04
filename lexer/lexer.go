a0369a8f21c50b18bf6f229e243e552808980552
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-04 22:30:40 +0300

    Properly format with gofmt

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -5,10 +5,10 @@ import (
 )
 
 type Lexer struct {
-	input string 
-	position int // current position in input
-	readPosition int // current reading position in input (after current char)
-	ch byte // current char under examination
+	input        string
+	position     int  // current position in input
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
 }
 
 func (l *Lexer) NextToken() token.Token {
